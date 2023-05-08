7021430993ac87b0780bdd3a0853cd122a26455e
Will Riffe <w_riffe@protonmail.com>
2023-05-08 12:31:13 -0400

    🚧 letting server.js stew, maybe done

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
-// Initialized Dependencies
+// Instantiate Dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-// Initialized Express
+// Instantiate Express
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Setting up some routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// For Error Handling
+const { validateNote } = require('./middlewares/validationMiddleware');
+const { errorHandler } = require('./middlewares/errorHandlerMiddleware');
+
+
+// Express Middleware
+/* ------------------------------- */
+
+// Parses requests with urlencoded payloads
+//Recognizes Request Object as Strings/Arrays
+app.use(express.urlencoded({ extended: true}));
+
+//Recognizes Request as JSON payload
+app.use(express.json());
+
+// Serves static files in "public" folder
+app.use(express.static("public"));
+
+/* ------------------------------- */
+ 
+// Routes
+app.use('/api', apiRoutes);
+app.use(htmlRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Start server
+app.listen(PORT, () => console.log(`Notes App Is Live at ${PORT}`));
