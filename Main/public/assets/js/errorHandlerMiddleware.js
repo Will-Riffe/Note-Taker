2708b3972b40742ddfaea259283ed750ad0aed21
Will Riffe <w_riffe@protonmail.com>
2023-07-02 21:41:02 -0400

    💥 Starting Over

--- Main/public/assets/js/errorHandlerMiddleware.js
+++
@@ -1,7 +0,0 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-  };
-  
-  module.exports = errorHandlerMiddleware;
-  
