3d4da2c3078316f247bdd604693157e4efa1ef04
Will Riffe <w_riffe@protonmail.com>
2023-07-03 08:56:39 -0400

    🚨 Set up app & PORT

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+// Dependencies
 const express = require("express");
 const apiRoute = require("./Main/routes/APIroute");
 const htmlRoute = require("./Main/routes/HTMLroute");
+
+// Set up app & port
+const app = express();
+const PORT = process.env.PORT || 3001;
 
